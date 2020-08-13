42ede6537081e7dcf10cb874086b7027511b1476
Chipnertkj <domekaij@gmail.com>
2020-08-13 19:55:28 +0200

    gdfbfgnbhfgn

---
+++ ChipBox/Discord.cpp
@@ -0,0 +1,2 @@
+#include "Discord.h"
+
---
+++ ChipBox/main.cpp
@@ -0,0 +1,515 @@
+// remember to remove anything labeled as debug before releasing
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>	// debug
+#include <string>
+#include <map>
+#include <algorithm>
+#include <chrono>
+#include "Discord.h"
+#include <vector>
+
+#define FILE "";
+
+char filename[128] = "";
+
+#pragma region Discord RPC
+
+void Discord::Initialize()
+{
+	DiscordEventHandlers Handle;
+	memset(&Handle, 0, sizeof(Handle));
+	Discord_Initialize("739852855338467420", &Handle, 1, NULL);
+}
+
+void Discord::Update()
+{
+	// filename workaround
+#undef FILE
+#define FILE filename;
+
+// update
+	DiscordRichPresence discordPresence;
+	memset(&discordPresence, 0, sizeof(discordPresence));
+	discordPresence.state = FILE;
+	discordPresence.details = "Editor";
+	std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
+	std::chrono::system_clock::duration dtn = tp.time_since_epoch();
+	discordPresence.startTimestamp = dtn.count() * std::chrono::system_clock::period::num / std::chrono::system_clock::period::den;
+	discordPresence.largeImageKey = "icon";
+	discordPresence.largeImageText = "ChipBox";
+	discordPresence.smallImageKey = "logo512";
+	discordPresence.smallImageText = FILE;
+	Discord_UpdatePresence(&discordPresence);
+}
+#pragma endregion
+
+#pragma region App Setup
+
+Discord * g_Discord;
+
+static std::string title = "ChipBox";
+static std::string version = "devbuild 7.29.20";
+
+int displayW = sf::VideoMode::getDesktopMode().width;
+int displayH = sf::VideoMode::getDesktopMode().height;
+int boxSize, pat;
+int popup = -1;
+float scale, out;
+bool fullscreen = false;
+bool drawReset, channelsUpdated = true;
+sf::Int64 dt;	// delta time
+
+sf::RenderWindow window(sf::VideoMode(300, 1), "Loading...");
+sf::View view = window.getView();
+sf::Event event;
+sf::Vector2u windowSize = window.getSize();
+
+sf::RenderTexture channelScroll;
+sf::RenderTexture channelPanel;
+sf::RenderTexture pianoKeys;
+sf::RenderTexture pianoRoll;
+
+sf::Vector2u channelScrollSize;
+sf::Vector2u channelPanelSize;
+sf::Vector2u pianoKeysSize;
+sf::Vector2u pianoRollSize;
+
+// usage : theme[c::bg] NOT theme[1]
+// for code readability
+enum c
+{
+	bg,
+	inside,
+	outside,
+	outline,
+	blank,
+	input,
+	light1,
+	light2,
+	dark,
+	selection,
+	keyC,
+	keyG,
+};
+
+
+// uint8_t range : 0-255
+// usage : theme[c::bg] NOT theme[1]
+// for code readability
+uint8_t theme[][3]
+{
+	{111, 116, 125},	// bg
+	{102, 105, 116},	// inside
+	{63, 66, 71},		// outside
+	{37, 37, 37},		// outline
+	{48, 48, 50},		// blank
+	{132, 136, 150},	// input
+	{255, 255, 255},	// light1
+	{132, 136, 150},	// light2
+	{0, 0, 0},			// dark
+	{76, 102, 204},		// selection
+	{100, 100, 100},	// keyC
+	{75, 75, 75},		// keyG
+};
+
+enum channelType
+{
+	synth,
+	noise,
+	sample,
+	mod
+};
+
+enum popups
+{
+	addChannel,
+};
+
+#pragma endregion
+
+#pragma region Classes
+
+class Instrument {
+	//wip
+};
+
+class Note {
+	std::vector<float> volume;
+	std::vector<float> pan;
+	int width;
+};
+
+class Pattern {
+private:
+	std::vector<std::map<int, Note>> notes;
+};
+
+class Channel {
+private:
+	uint8_t cType;
+public:
+	std::string cName;
+	int instrument = 1;
+	std::vector<int> slots;
+	std::map<int, Pattern> patterns;
+	std::vector<Instrument> instruments;
+
+	Channel(std::string name, uint8_t type)
+	{
+		cName = name;
+		cType = type;
+	};
+};
+
+#pragma endregion
+
+#pragma region Class Setup
+
+std::vector<Channel> channels;
+
+#pragma endregion
+
+
+#pragma region Utility functions
+float approach(int a, int b, int c)
+{
+	// debug
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::F3))
+		std::cout << "\t" << a;
+	// debug
+
+	if (a < b)
+		a += c;
+	if (a > b)
+		a -= c;
+
+	// debug
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::F3)) {
+		std::cout << " && " << c << " = " << a << std::endl;
+		if (a == b)
+			std::cout << "[appr]\ta == " << b << std::endl;
+	}
+	// debug
+
+	return a;
+}
+
+// Sets some vars relatively to display size
+void displaySetup(bool reset)
+{
+	// window reset to default or fullscreen size
+	if (reset) {
+		displayW = sf::VideoMode::getDesktopMode().width;
+		displayH = sf::VideoMode::getDesktopMode().height;
+		int w = displayW;
+		int h = displayH;
+		if (!fullscreen) {
+			w = (int)((float)displayW*0.75);
+			h = (int)((float)displayH*0.75);
+			window.create(sf::VideoMode(w, h), title + " | " + version, sf::Style::Close);
+		}
+		else {
+			window.create(sf::VideoMode(w, h), title + " | " + version, sf::Style::Fullscreen);
+		}
+	}
+
+	window.setVerticalSyncEnabled(true);
+
+	windowSize = window.getSize();
+	if (reset)
+		view.setSize(windowSize.x, windowSize.y);
+	boxSize = std::min(windowSize.x, windowSize.y) / 3.5f;
+	out = boxSize / 50;
+	scale = (float)out / 7;
+	pat = std::ceil(boxSize / 7);
+
+	drawReset = true;
+}
+
+// creates and returns an sfml color value using the current theme
+sf::Color color(uint8_t index, uint8_t alpha)
+{
+	return sf::Color(theme[index][0], theme[index][1], theme[index][2], alpha);
+}
+
+bool button(sf::Vector2f p, sf::Vector2f& s, bool centered) {
+	if (centered) {
+		p.x -= s.x / 2;
+		p.y -= s.y / 2;
+	}
+
+	sf::Vector2i pos = sf::Mouse::getPosition(window);
+
+	if (pos.x > p.x) {
+		if (pos.x < p.x + s.x) {
+			if (pos.y > p.y) {
+				if (pos.y < p.y + s.y) {
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+
+// setup clear color for render target clearing
+sf::Color clear = color(c::bg, 255);
+#pragma endregion
+
+void keyPressed(sf::Keyboard::Key key)
+{
+	if (key == sf::Keyboard::F11) {
+		fullscreen = !fullscreen;
+		displaySetup(true);
+	}
+}
+
+void keyReleased(sf::Keyboard::Key key)
+{
+
+}
+
+// handles SFML events
+void eventHandler()
+{
+	// if you don't know what this does you should probably watch some SFML tutorials first idiot
+	while (window.pollEvent(event))
+	{
+		switch (event.type) {
+		default:
+			break;
+		case sf::Event::EventType::Closed:
+			window.close(); break;
+		case sf::Event::KeyPressed:
+			keyPressed(event.key.code); break;
+		case sf::Event::KeyReleased:
+			keyReleased(event.key.code); break;
+		case sf::Event::Resized:
+			view.setSize(event.size.width, event.size.height);
+			window.setSize(sf::Vector2u(event.size.width, event.size.height));
+			displaySetup(false); break;
+		}
+	}
+}
+
+#pragma region main
+// main
+int main()
+{
+	// rpc
+	g_Discord->Initialize();
+	g_Discord->Update();
+
+	// init
+	displaySetup(true);
+	sf::Clock clock;
+
+	// delta
+	int v;
+	float val;
+
+	std::map<std::string, int> delta;
+	delta["addH"] = 0.0f;
+	delta["channels"] = 100.0f;
+
+	// textures
+	sf::Texture t_plus;
+	sf::Texture t_highlight1;
+	sf::Texture t_highlight2;
+
+	t_plus.loadFromFile("images/plus.png");
+	t_highlight1.loadFromFile("images/highlight1.png");
+	t_highlight2.loadFromFile("images/highlight2.png");
+
+	// fonts
+	sf::Font f_timeburner;
+
+	f_timeburner.loadFromFile("fonts/timeburnernormal.ttf");
+
+	// strings
+	std::string hover;
+	std::string hoverLast;
+
+	const std::string hoverAdd = "add";
+
+	// drawables
+	// add box
+	sf::RectangleShape dr_boxb;
+	sf::RectangleShape dr_boxf;
+	sf::Sprite bt_add(t_plus);
+	sf::Sprite bt_addH1(t_highlight1);
+	sf::Sprite bt_addH2(t_highlight2);
+	sf::Text tx_add1("Add a new", f_timeburner);
+	sf::Text tx_add2("channel", f_timeburner);
+	// channels
+	sf::RectangleShape dr_channelsb;
+	sf::RectangleShape dr_channelsf;
+	// popups
+	sf::RectangleShape dr_popupbg;
+	sf::RectangleShape dr_popupb;
+	sf::RectangleShape dr_popupf;
+	sf::RectangleShape dr_popupInput;
+	sf::Text tx_popupInput("Channel name:", f_timeburner);
+
+	// drawables' vars
+	sf::Vector2f bt_add_p;
+	sf::Vector2f bt_add_s;
+	sf::Vector2u highlight_size = t_highlight1.getSize();
+
+	// loop
+	while (window.isOpen())
+	{
+		// reset drawables
+		if (drawReset) {
+			// add box
+			// dr_boxb
+			dr_boxb.setPosition(sf::Vector2f(windowSize.x - boxSize, windowSize.y - boxSize));
+			dr_boxb.setSize(sf::Vector2f(boxSize, boxSize));
+			dr_boxb.setFillColor(color(c::outside, 255));
+			dr_boxb.setOutlineColor(color(c::outline, 255));
+			dr_boxb.setOutlineThickness(-1);
+			// dr_boxf
+			dr_boxf.setPosition(sf::Vector2f(windowSize.x - boxSize + out, windowSize.y - boxSize + out));
+			dr_boxf.setSize(sf::Vector2f(boxSize - out * 2.0f, boxSize - out * 7.0f));
+			dr_boxf.setFillColor(color(c::inside, 255));
+			dr_boxf.setOutlineColor(color(c::outline, 255));
+			dr_boxf.setOutlineThickness(-1);
+			// bt_add
+			sf::Vector2u plus_size = t_plus.getSize();
+			bt_add.setColor(color(c::light1, 110));
+			bt_add.setScale(sf::Vector2f(scale, scale));
+			bt_add.setOrigin(sf::Vector2f((float)plus_size.x / 2.0f * scale, (float)plus_size.y / 2.0f * scale));
+			bt_add.setPosition(sf::Vector2f(std::floor((float)windowSize.x - boxSize / 2.0f), std::floor((float)windowSize.y - out * 3.5f)));
+			bt_add_p.x = (float)windowSize.x - boxSize / 2.0f;
+			bt_add_p.y = (float)windowSize.y - out * 3.5f;
+			bt_add_s.x = (float)plus_size.x * scale * 5.0f;
+			bt_add_s.y = (float)plus_size.x * scale * 2.0f;
+			// bt_addH1
+			bt_addH1.setColor(color(c::light1, 0));
+			bt_addH1.setPosition(sf::Vector2f(std::ceil((float)windowSize.x - boxSize / 2.0f), std::floor((float)windowSize.y - out * 3.2f)));
+			bt_addH1.setOrigin(sf::Vector2f(std::ceil(highlight_size.x / 2.0f), std::ceil(highlight_size.y / 2.0f)));
+			bt_addH1.setScale(sf::Vector2f(scale / 1.3f, scale / 1.3f));
+			// bt_addH2
+			bt_addH2.setColor(color(c::light1, 0));
+			bt_addH2.setPosition(sf::Vector2f(std::ceil((float)windowSize.x - boxSize / 2.0f), std::floor((float)windowSize.y - out * 3.2f)));
+			bt_addH2.setOrigin(sf::Vector2f(std::ceil(highlight_size.x / 2.0f), std::ceil(highlight_size.y / 2.0f)));
+			bt_addH2.setScale(sf::Vector2f(scale / 1.3f, scale / 1.3f));
+			// tx_add1
+			tx_add1.setCharacterSize(scale * 50);
+			sf::FloatRect textRect = tx_add1.getLocalBounds();
+			tx_add1.setOrigin(std::ceil(textRect.left + textRect.width / 2.0f), std::ceil(textRect.top + textRect.height / 2.0f));
+			tx_add1.setFillColor(color(c::light1, 255));
+			tx_add1.setPosition(sf::Vector2f(std::ceil((float)windowSize.x - boxSize / 2.0f), std::ceil((float)windowSize.y - boxSize / 2.0f - textRect.height)));
+			// tx_add1
+			tx_add2.setCharacterSize(scale * 50);
+			textRect = tx_add2.getLocalBounds();
+			tx_add2.setOrigin(std::ceil(textRect.left + textRect.width / 2.0f), std::ceil(textRect.top + textRect.height / 2.0f));
+			tx_add2.setFillColor(color(c::light1, 255));
+			tx_add2.setPosition(sf::Vector2f(std::ceil((float)windowSize.x - boxSize / 2.0f), std::ceil((float)windowSize.y - boxSize / 2.0f + textRect.height / 1.75f)));
+			// channels
+			// dr_channelsb
+			dr_channelsb.setPosition(sf::Vector2f(windowSize.x * delta["channels"], windowSize.y - boxSize));
+			dr_channelsb.setSize(sf::Vector2f(windowSize.x - boxSize, boxSize));
+			dr_channelsb.setFillColor(color(c::outside, 255));
+			dr_channelsb.setOutlineColor(color(c::outline, 255));
+			dr_channelsb.setOutlineThickness(-1);
+			// dr_channelsf
+			dr_channelsf.setPosition(sf::Vector2f(windowSize.x * delta["channels"] + out, windowSize.y - boxSize + out));
+			dr_channelsf.setSize(sf::Vector2f(windowSize.x - boxSize - out * 2.0f, boxSize - out * 7.0f));
+			dr_channelsf.setFillColor(color(c::inside, 255));
+			dr_channelsf.setOutlineColor(color(c::outline, 255));
+			dr_channelsf.setOutlineThickness(-1);
+
+			drawReset = false;
+		}
+
+		// handle events
+		eventHandler();
+
+		// clear
+		hoverLast = hover;
+		hover = "";
+		window.clear(clear);
+#pragma endregion
+#pragma region Update
+
+		// bt_add button
+		if (button(bt_add_p, bt_add_s, true)) {
+			hover = hoverAdd;
+		}
+
+		if (hoverLast != hover) {
+			if (hover == hoverAdd)
+				bt_add.setColor(color(c::light1, 255));
+			else
+				bt_add.setColor(color(c::light1, 110));
+		}
+
+		if (hoverLast != hover || (delta["addH"] > 0 && delta["addH"] < 100)) {
+			if (hover == hoverAdd)
+				v = 100;
+			else
+				v = 0;
+
+			delta["addH"] = approach(delta["addH"], v, std::ceil(std::abs((float)delta["addH"] - v) / 2.0f));
+			val = (float)delta["addH"] / 100.0f;
+			sf::Color cc = color(c::light1, 255 * val);
+			bt_addH1.setScale(scale / 1.3f * val, scale / 1.3f);
+			bt_addH2.setScale(scale / 1.3f * val, scale / 1.3f);
+			bt_addH1.setColor(cc);
+			bt_addH2.setColor(cc);
+		}
+
+		// channel box
+		if (channelsUpdated) {
+			if (channels.size() > 0)
+				v = 0;
+			else
+				v = 100;
+
+			delta["channels"] = approach(delta["channels"], v, std::ceil(std::abs((float)delta["channels"] - v) / 6.0f));
+			val = (float)delta["channels"] / 100.0f;
+			dr_channelsb.setPosition(sf::Vector2f(windowSize.x * val, windowSize.y - boxSize));
+			dr_channelsf.setPosition(sf::Vector2f(windowSize.x * val + out, windowSize.y - boxSize + out));
+
+			if (delta["channels"] == 0 || delta["channels"] == 100)
+				channelsUpdated = false;
+		}
+
+#pragma endregion
+
+		// ########  ########     ###    ##      ## 
+		// ##     ## ##     ##   ## ##   ##  ##  ## 
+		// ##     ## ##     ##  ##   ##  ##  ##  ## 
+		// ##     ## ########  ##     ## ##  ##  ## 
+		// ##     ## ##   ##   ######### ##  ##  ## 
+		// ##     ## ##    ##  ##     ## ##  ##  ## 
+		// ########  ##     ## ##     ##  ###  ###
+
+		// channels
+		window.draw(dr_channelsb);
+		window.draw(dr_channelsf);
+
+		// add box
+		window.draw(dr_boxb);
+		window.draw(dr_boxf);
+		window.draw(tx_add1);
+		window.draw(tx_add2);
+		window.draw(bt_add);
+		window.draw(bt_addH1);
+		window.draw(bt_addH2);
+
+#pragma region main
+		window.display();
+
+		// clock restart
+		dt = clock.getElapsedTime().asMicroseconds();
+		clock.restart();
+	}
+
+	// end
+	return 0;
+}
+#pragma endregion
